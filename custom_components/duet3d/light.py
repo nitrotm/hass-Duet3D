511c5e1a31408e26b614a09e38aa28df656257ed
Antony Ducommun <antony@aducommun.ch>
2026-03-28 14:21:24 +0100

    Fix for hass 2026-03 (cf. demonio669 patch)

--- custom_components/duet3d/light.py
+++ custom_components/duet3d/light.py
@@ -1,32 +1,29 @@
 import logging
-
+import colorsys
 import voluptuous as vol
 
 from homeassistant.components.light import (
     ATTR_BRIGHTNESS,
-    PLATFORM_SCHEMA,
+    ATTR_HS_COLOR,
+    ColorMode,
     LightEntity,
-    SUPPORT_COLOR,
 )
-import homeassistant.helpers.config_validation as cv
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
-from homeassistant.config_entries import ConfigEntry
-import colorsys
 
 from . import DuetDataUpdateCoordinator
+from .const import (
+    ATTR_GCODE,
+    CONF_LIGHT,
+    CONF_STANDALONE,
+    DOMAIN,
+    SERVICE_SEND_GCODE,
+)
 
-from .const import CONF_NAME, ATTR_GCODE, DOMAIN, SERVICE_SEND_GCODE, CONF_LIGHT, CONF_STANDALONE
 
 _LOGGER = logging.getLogger(__name__)
-
-# Define the validation schema for the platform configuration
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
-    {
-        vol.Required("name"): cv.string,
-    }
-)
 
 
 async def async_setup_entry(
@@ -35,58 +32,36 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ):
     """Set up the Duet3D light platform."""
-    if config_entry.data[CONF_STANDALONE]:
+    if config_entry.data.get(CONF_STANDALONE):
         return
-    lightIncluded = config_entry.data[CONF_LIGHT]
-    if lightIncluded:
-        coordinator: DuetDataUpdateCoordinator = hass.data[DOMAIN][
-            config_entry.entry_id
-        ]["coordinator"]
+
+    if config_entry.data.get(CONF_LIGHT):
+        coordinator: DuetDataUpdateCoordinator = hass.data[DOMAIN][config_entry.entry_id]["coordinator"]
         device_id = config_entry.entry_id
         assert device_id is not None
-        entities: list[LightEntity] = [
-            Duet3DLight(coordinator, "LED", device_id),
-        ]
-        async_add_entities(entities)
+        async_add_entities([Duet3DLight(coordinator, "LED", device_id)])
 
 
-class Duet3DLightBase(CoordinatorEntity[DuetDataUpdateCoordinator], LightEntity):
+class Duet3DLight(CoordinatorEntity[DuetDataUpdateCoordinator], LightEntity):
     """Representation of a light connected to a Duet3D printer."""
-
-    def __init__(
-        self, coordinator: DuetDataUpdateCoordinator, light_name: str, device_id: str
-    ) -> None:
-        """Initialize the light."""
+    def __init__(self, coordinator, name, device_id):
         super().__init__(coordinator)
         self._device_id = device_id
-        self._attr_name = f"{self.device_info['name']} {light_name}"
-        self._attr_unique_id = device_id
-
-    @property
-    def device_info(self):
-        """Device info."""
-        return self.coordinator.device_info
-
-
-class Duet3DLight(Duet3DLightBase):
-    def __init__(
-        self, coordinator: DuetDataUpdateCoordinator, name: str, device_id: str
-    ) -> None:
-        super().__init__(coordinator, name, f"{name}-{device_id}")
+        self._attr_name = name
+        self._attr_unique_id = f"{device_id}_light_{name}"
+        self._attr_has_entity_name = True
+        self._attr_should_poll = False
+        self._attr_supported_color_modes = {ColorMode.RGB}
+        self._attr_color_mode = ColorMode.RGB
         self._state = False
         self._brightness = 255
         self._rgb_color = (255, 255, 255)
         self._last_brightness = self._brightness
 
     @property
-    def name(self):
-        """Return the name of the light."""
-        return self._attr_name
-
-    @property
-    def should_poll(self):
-        """No polling needed for a Duet3D light."""
-        return False
+    def device_info(self):
+        """Device info."""
+        return self.coordinator.device_info
 
     @property
     def is_on(self):
@@ -97,11 +72,6 @@ class Duet3DLight(Duet3DLightBase):
     def brightness(self):
         """Return the brightness of the light."""
         return self._brightness
-
-    @property
-    def supported_features(self):
-        """Flag supported features."""
-        return SUPPORT_COLOR
 
     @property
     def rgb_color(self):
@@ -119,14 +89,13 @@ class Duet3DLight(Duet3DLightBase):
         if ATTR_BRIGHTNESS in kwargs:
             self._brightness = kwargs[ATTR_BRIGHTNESS]
             self._last_brightness = self._brightness
+        else:
+            # Use the last brightness value if it was not passed in the service call
+            self._brightness = self._last_brightness
 
         # Set the RGB color if it was passed in the service call
-        if "hs_color" in kwargs:
-            self._rgb_color = self._hs_to_rgb(kwargs["hs_color"])
-
-        # Use the last brightness value if it was not passed in the service call
-        if ATTR_BRIGHTNESS not in kwargs:
-            self._brightness = self._last_brightness
+        if ATTR_HS_COLOR in kwargs:
+            self._rgb_color = self._hs_to_rgb(kwargs[ATTR_HS_COLOR])
 
         # Build the M150 GCode command
         command = "M150 R{} U{} B{} P{}".format(
@@ -134,19 +103,10 @@ class Duet3DLight(Duet3DLightBase):
         )
 
         # Call the send_code service to send the M150 GCode to the Duet3D board
-        try:
-            await self.hass.services.async_call(
-                DOMAIN,
-                SERVICE_SEND_GCODE,
-                {
-                    ATTR_GCODE: command,
-                },
-            )
-        except Exception as e:
-            _LOGGER.error("Error calling send_gcode service: %s", e)
+        await self._send_command_to_printer(command)
 
         # Update the light state in Home Assistant
-        self.async_schedule_update_ha_state()
+        self.async_write_ha_state()
 
     async def async_turn_off(self, **kwargs):
         """Turn the light off."""
@@ -159,16 +119,19 @@ class Duet3DLight(Duet3DLightBase):
         command = "M150 R0 U0 B0 P0"
 
         # Call the send_code service to send the M150 GCode to the Duet3D board
+        await self._send_command_to_printer(command)
+
+        # Update the light state in Home Assistant
+        self.async_write_ha_state()
+
+    async def _send_command_to_printer(self, command):
+        """Internal method to call the G-code service."""
         try:
             await self.hass.services.async_call(
                 DOMAIN,
                 SERVICE_SEND_GCODE,
-                {
-                    ATTR_GCODE: command,
-                },
+                {ATTR_GCODE: command},
+                blocking=True # Esperamos a que se envíe
             )
         except Exception as e:
-            _LOGGER.error("Error calling send_gcode service: %s", e)
-
-        # Update the light state in Home Assistant
-        self.async_schedule_update_ha_state()
+            _LOGGER.error("Error al enviar G-code a Duet3D: %s", e)
